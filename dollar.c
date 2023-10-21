710bd1b2677d7f0b7dd4c558db633eb3bc1c0e35
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-21 16:24:42 +0500

    add comments

--- dollar.c
+++ dollar.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
+//The program about to exchange dollar to pound and yen
 int main(){
+    //variable
     float dollar, fee, total_dollar, pound;
     int yen; 
+    //prompt to user
     printf("Enter the amount of dollar:");
     scanf("%f",&dollar);
+    //exchange fee and half dollar conver to yen and half to pound
     fee=dollar * 0.1;
     total_dollar=dollar-fee;
     pound=(total_dollar/2) * 0.79;
     yen= (total_dollar/2) * 127.65;
+    // output
     printf("Half dollar exchange to %f GBP\n",pound);
     printf("Half dollar convert to %d JPY\n",yen);
     return 0;
