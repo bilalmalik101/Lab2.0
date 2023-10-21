9fa14da78257cd050809757fc9b44017145cf866
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-21 16:21:38 +0500

    take 10% exchange fee and then convert the half to dollar and half to yen

--- dollar.c
+++ dollar.c
@@ -4,4 +4,11 @@ int main(){
     int yen; 
     printf("Enter the amount of dollar:");
     scanf("%f",&dollar);
+    fee=dollar * 0.1;
+    total_dollar=dollar-fee;
+    pound=(total_dollar/2) * 0.79;
+    yen= (total_dollar/2) * 127.65;
+    printf("Half dollar exchange to %f GBP\n",pound);
+    printf("Half dollar convert to %d JPY\n",yen);
+    return 0;
 }
